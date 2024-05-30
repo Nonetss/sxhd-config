94256a499b9b3edf6857604a7974a7416e6ecfc6
Antonio Moreno <amorenolopezbarajas@gmail.com>
2024-05-30 02:51:53 +0200

    Rofi para layout mucho mas chingon

--- scripts/cambiar_layout.sh
+++ scripts/cambiar_layout.sh
@@ -5,7 +5,7 @@ CONFIG_DIR="$HOME/.config/sxhkd/scripts"
 ICON_DIR="$CONFIG_DIR/layouts"
 ROFI_CONFIG="$CONFIG_DIR/config-rofi/config.rasi"
 
-# Opciones de layout con iconos (usando el formato de iconos de rofi)
+# Opciones de layout con iconos
 LAYOUTS=("tall:$ICON_DIR/tall.jpeg"
          "rtall:$ICON_DIR/rtall.jpeg"
          "wide:$ICON_DIR/wide.jpeg"
@@ -27,36 +27,46 @@ done
 # Selección de layout usando rofi
 SELECTED_LAYOUT=$(echo -e "$MENU" | rofi -dmenu -theme "$ROFI_CONFIG" -p "Selecciona un layout" -format "i s" | awk '{print $2}')
 
-# Cambiar el layout en bspwm
+# Cambiar el layout en bspwm y actualizar la imagen de fondo
 case $SELECTED_LAYOUT in
     tall)
         bsp-layout set tall
+        feh --bg-scale "$ICON_DIR/tall.jpeg"
         ;;
     rtall)
         bsp-layout set rtall
+        feh --bg-scale "$ICON_DIR/rtall.jpeg"
         ;;
     wide)
         bsp-layout set wide
+        feh --bg-scale "$ICON_DIR/wide.jpeg"
         ;;
     rwide)
         bsp-layout set rwide
+        feh --bg-scale "$ICON_DIR/rwide.jpeg"
         ;;
     grid)
         bsp-layout set grid
+        feh --bg-scale "$ICON_DIR/grid.jpeg"
         ;;
     rgrid)
         bsp-layout set rgrid
+        feh --bg-scale "$ICON_DIR/rgrid.jpeg"
         ;;
     even)
         bsp-layout set even
+        feh --bg-scale "$ICON_DIR/even.jpeg"
         ;;
     tiled)
         bsp-layout set tiled
+        feh --bg-scale "$ICON_DIR/tiled.jpeg"
         ;;
     monocle)
         bsp-layout set monocle
+        feh --bg-scale "$ICON_DIR/monocle.jpeg"
         ;;
     *)
         echo "Layout no válido"
         ;;
 esac
+
